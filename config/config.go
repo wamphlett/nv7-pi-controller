53ad2a0f9c49981d6360c4b17ef14bf8b41b1677
Warren Amphlett <warrenamphlett@gmail.com>
2023-05-13 13:29:17 +0100

    update the config with channel a targets

--- config/config.go
+++ config/config.go
@@ -29,10 +29,10 @@ type Config struct {
 type Controller struct {
 	LEDPin int `env:"CONTROLLER_LED_PIN,default=13"`
 
-	ChannelTarget []int `env:"CONTROLLER_CHANNEL_TARGET,default=61280"`
-	ModeTarget    []int `env:"CONTROLLER_MODE_TARGET,default=62180"`
-	ColorTarget   []int `env:"CONTROLLER_COLOR_TARGET,default=64000"`
-	SpeedTarget   []int `env:"CONTROLLER_SPEED_TARGET,default=62950"`
+	ChannelTarget []int `env:"CONTROLLER_CHANNEL_TARGET,default=61280,64660"`
+	ModeTarget    []int `env:"CONTROLLER_MODE_TARGET,default=62180,65200"`
+	ColorTarget   []int `env:"CONTROLLER_COLOR_TARGET,default=64000,760"`
+	SpeedTarget   []int `env:"CONTROLLER_SPEED_TARGET,default=62950,65520"`
 
 	Tolerance int `env:"CONTROLLER_TOLERANCE,default=40"`
 }
